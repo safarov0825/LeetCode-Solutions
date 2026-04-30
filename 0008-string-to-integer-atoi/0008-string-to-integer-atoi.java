697ca3a6698a68685801c531cea22279f0d41291
Iskandar Safarov <safarov9291@gmail.com>
2026-04-30 21:59:13 +0500

    Time: 1 ms (100%), Space: 43.8 MB (41.99%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int myAtoi(String s) {
+        long x = 0;
+        int sign = 1;
+        boolean found = false;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 47 && c < 58) {
+                found = true;
+                x = 10 * x + ((int) c - 48);
+                if (x * sign > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+                if (x * sign < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+            } else if (c == '-') {
+                if (found) {
+                    break;
+                }
+                found = true;
+                sign *= -1;
+            } else if (c == ' ') {
+                if (found) {
+                    break;
+                }
+            } else if (c == '+') {
+                if (found) {
+                    break;
+                }
+                found = true;
+            } else {
+                break;
+            }
+        }
+        long x2 = x * sign;
+        int limit = -1 * (int) Math.pow(2, 31) - 1;
+
+        if (x2 > (limit * (-1) - 1)) {
+            return (limit * (-1) - 1);
+        } else if (x2 < limit) {
+            return limit;
+        }
+        return (int) x2;
+    }
+}
