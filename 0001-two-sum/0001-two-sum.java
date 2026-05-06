48c7a562215686d3abfb89be87a5e3c687737e4f
Iskandar Safarov <safarov9291@gmail.com>
2026-05-07 01:21:52 +0500

    Time: 33 ms (40.86%), Space: 47 MB (58.3%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] solution = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int second = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == second) {
+                    solution[0] = i;
+                    solution[1] = j;
+                }
+            }
+        }
+        return solution;
+    }
+}
