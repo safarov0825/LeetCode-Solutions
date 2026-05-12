598ab141fa07f6c16cfe7454434a6034ee588708
Iskandar Safarov <safarov9291@gmail.com>
2026-05-12 22:39:52 +0500

    Time: 56 ms (5.04%), Space: 42.1 MB (97.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,10 @@
 class Solution {
     public int mySqrt(int x) {
-        int i = 0;
         int num = 1;
         while (num <= x / num) {
-            i++;
             num++;
         }
 
-        return i;
+        return num - 1;
     }
 }
