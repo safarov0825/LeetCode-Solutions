bb713d73ec3aface5abd65639bdf55782a279b81
Iskandar Safarov <safarov9291@gmail.com>
2026-08-22 20:32:17 +0500

    Time: 31 ms (39.44%), Space: 25.6 MB (92.56%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        seen = set()
+        for n in nums:
+            if n in seen:
+                return True
+            seen.add(n)
+        return False
+        
