dd9fa823d669a0f4d18f59c30828594fe13b3ef3
Iskandar Safarov <safarov9291@gmail.com>
2026-08-27 00:37:10 +0500

    Time: 7 ms (17.7%), Space: 20.5 MB (8.51%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -5,8 +5,6 @@ class Solution(object):
         :rtype: None Do not return anything, modify s in-place instead.
         """
         for i in range(len(s) // 2):
-            temp = s[i]
-            s[i] = s[len(s) - i - 1]
-            s[len(s) - i - 1] = temp
+            s[i], s[len(s) - i - 1] = s[len(s) - i - 1], s[i]
 
         
