88802e8ce69c330b3c04af3095c16f969ed11ddd
Iskandar Safarov <safarov9291@gmail.com>
2026-08-27 17:27:32 +0500

    Time: 36 ms (61.3%), Space: 20 MB (10.2%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        seen = set()
+
+        current = head
+        while current is not None:
+            if current in seen:
+                return True
+            seen.add(current)
+            current = current.next
+
+        return False
+        
